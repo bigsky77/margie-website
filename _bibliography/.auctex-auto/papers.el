04b2f62588751efee8528e9f00025786c72f9764
bigsky77 <simonjudd2@gmail.com>
2024-01-08 17:32:56 -0700

    fix formating

---
+++ _bibliography/.auctex-auto/papers.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "gregor2015draw"
+    "einstein1956investigations"
+    "einstein1950meaning"
+    "PhysRev.47.777"
+    "einstein1905molekularkinetischen"
+    "einstein1905movement"
+    "einstein1905electrodynamics"
+    "przibram1967letters"))
+ '(or :bibtex :latex))
+
