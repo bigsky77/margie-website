b28f49f074b9e88201037f2cc245c9bbdc624c4f
bigsky77 <simonjudd2@gmail.com>
2024-08-01 11:55:58 -0600

    update photo and bibliography links

--- _bibliography/.auctex-auto/papers.el
+++ _bibliography/.auctex-auto/papers.el
@@ -2,13 +2,6 @@
  "papers"
  (lambda ()
    (LaTeX-add-bibitems
-    "gregor2015draw"
-    "einstein1956investigations"
-    "einstein1950meaning"
-    "PhysRev.47.777"
-    "einstein1905molekularkinetischen"
-    "einstein1905movement"
-    "einstein1905electrodynamics"
-    "przibram1967letters"))
+    "gregor2015draw"))
  '(or :bibtex :latex))
 
